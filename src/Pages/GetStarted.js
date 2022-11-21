a00b718e8c4fd0a0888301a9ea7593add9b7904b
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-11-20 22:02:38 -0500

    Update GetStarted.js

--- src/Pages/GetStarted.js
+++ src/Pages/GetStarted.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import { Grid, Box } from "@material-ui/core";
-import Title from "../Assets/images/logo.png";
+import Title from "../Assets/images/imti.png";
 import GetStartedImg from "../Assets/images/BEGIN.png";
 import Stars from "../Components/Stars/Stars";
 
