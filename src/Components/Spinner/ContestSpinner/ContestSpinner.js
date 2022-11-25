eb41044edfd84c293ce46e9681211a8c8ea75248
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-11-25 16:07:39 -0500

    Delete src directory

--- src/Components/Spinner/ContestSpinner/ContestSpinner.js
+++
@@ -1,9 +0,0 @@
-import classes from "./ContestSpinner.module.css";
-
-export default function Spinner(props) {
-  return (
-    <div className={classes.wraped} style={{ marginTop: props.marginTop }}>
-      <div className={classes.spinneer}></div>
-    </div>
-  );
-}
--- src/Components/Spinner/EditorSpinner/EditorSpinner.js
+++
@@ -1,9 +0,0 @@
-import "./EditorSpinner.css";
-
-export default function Spinner() {
-  return (
-    <div class="spinnerr">
-      <div class="halfSpin"></div>
-    </div>
-  );
-}
